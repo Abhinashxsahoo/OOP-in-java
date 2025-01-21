b814487812fd32360c9e2daa6cc6056f5ac149ae
Abhinashxsahoo <161676152+Abhinashxsahoo@users.noreply.github.com>
2025-01-21 16:58:37 +0530

    Add files via upload

---
+++ experiment2Java/Pattern2.java
@@ -0,0 +1,20 @@
+package experiment2Java;
+
+import java.util.Scanner;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of lines: ");
+        int n = scanner.nextInt();
+        System.out.print("Pattern: \n");
+        for (int i = n; i >= 1; i--) {
+            char ch = 'A';
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
