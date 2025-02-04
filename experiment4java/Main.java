feffed33b05820e8400ea19c357aa26e0ac32857
Abhinash Sahoo <161676152+Abhinashxsahoo@users.noreply.github.com>
2025-02-04 15:48:38 +0530

    Add files via upload

---
+++ experiment4java/Main.java
@@ -0,0 +1,31 @@
+class Car {
+
+    String make;
+    String model;
+    int year;
+    String color;
+
+    public Car(String make, String model, int year, String color) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+
+    public void displayDetails() {
+        System.out.println("Car Details:");
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Color: " + color);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car myCar = new Car("LAMBORGHINI", "URUS", 2022, "BLACK");
+
+        myCar.displayDetails();
+    }
+}
